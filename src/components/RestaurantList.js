9580a67e14ba853826a0782213c367e68f1c16c1
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-01 00:18:24 -0500

    Added rating components, need to fix functionality

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { List, Card} from 'antd';
-
+import React, { useState, useEffect } from "react";
+import { List, Card, Rate } from "antd";
+import RestaurantRating from "./RestaurantRating";
 
 export default function RestaurantList() {
   const [restaurants, setRestaurants] = useState();
@@ -18,20 +18,22 @@ export default function RestaurantList() {
   return (
     <List
       dataSource={restaurants}
-      renderItem={(item) => (
+      renderItem={(restaurant) => (
         <List.Item>
-          <Card 
-            title={item.name}
+          <Card
+            headStyle={{ textAlign: "left", fontSize: 20, padding: 0 }}
+            title={restaurant.name}
             hoverable
-            style={{ width: '80%' }}
-            cover={<img alt="Restaurant" src={item.photoUrl} />}
+            style={{ width: "400px" }}
+            cover={<img alt={restaurant.name} src={restaurant.photoUrl} />}
           >
-            <p>Rating</p>
-            <p>Rate this restaurant</p>
+            <Rate disabled allowHalf defaultValue={restaurant.rating} />
+            <span> ({restaurant.numRatings})</span>
+            <p>{restaurant.address}</p>
+            <RestaurantRating />
           </Card>
         </List.Item>
       )}
     />
   );
 }
-
--- src/components/RestaurantRating.js
+++ src/components/RestaurantRating.js
@@ -1,10 +1,94 @@
-import { List } from 'antd';
-import Rating from './Rating'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { Rate, Button, Cascader } from 'antd';
+
+const options = [
+  {
+    value: 5,
+    label: "5",
+  },
+  {
+    value: 4.5,
+    label: "4.5",
+  },
+  {
+    value: 4,
+    label: "4",
+  },
+  {
+    value: 3.5,
+    label: "3.5",
+  },
+  {
+    value: 3,
+    label: "3",
+  },
+  {
+    value: 2.5,
+    label: "2.5",
+  },
+  {
+    value: 2,
+    label: "2",
+  },
+  {
+    value: 1.5,
+    label: "1.5",
+  },
+  {
+    value: 1,
+    label: "1",
+  },
+  {
+    value: 0.5,
+    label: "0.5",
+  },
+];
 
 export default function RestaurantRating () {
+    const [restaurant, setRestaurant] = useState({}); // initial state set to null?
+    const [rating, setRating] = useState();
+    const params = useParams();
+
+    const handleRating = () => {
+        fetch(`https://bocacode-intranet-api.web.app/restaurants/${params.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ rating: rating }),
+        })
+        .then((response) => response.json)
+        .then(() => setRating(0))
+        .catch(alert);
+    };
+
+    useEffect(() => {
+        fetch(`https://bocacode-intranet-api.web.app/restaurants`)
+            .then((response) => response.json())
+            .then((data) => setRestaurant(data))
+            .catch(alert);
+    }, [rating]);
+
+    
+
   return (
-    <List
-      renderItem={item => <Rating item={item} />}
-    />
+    <div className="rating">
+      {/* <Rate value={rating} onChange={(value) => setRating(value)} /> */}
+      <Cascader
+        defaultValue={["Give it a rating"]}
+        options={options}
+        onChange={(value) => setRating(value)}
+      />
+      <br></br>
+      <Button
+        onClick={handleRating}
+        type="primary"
+        size={"large"}
+        style={{ marginTop: "20px" }}
+      >
+        Submit Rating
+      </Button>
+    </div>
   );
 }
